195e5c62c311cbeb1803864149765241904ac866
almabarraza <abarrazaraygoza@byupathway.edu>
2025-09-20 10:19:36 -0700

    week 03 team activity is done

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const listElement = document.getElementById("listElement");
 const productList = new ProductList("tents", dataSource, listElement);
+
+loadHeaderFooter();
 
 productList.init();
