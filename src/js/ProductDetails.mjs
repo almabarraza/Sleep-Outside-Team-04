b721d5340f14ed0977f71383fd5b37b473287635
almabarraza <abarrazaraygoza@byupathway.edu>
2025-09-23 14:38:26 -0700

    week 3 individual activity is done

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -11,6 +11,7 @@ export default class ProductDetails {
     async init() {
 
         this.product = await this.dataSource.findProductById(this.productId);
+        this.renderProductDetails();
 
         document.getElementById('addToCart').addEventListener('click', this.addProductToCart.bind(this));
 
@@ -26,27 +27,43 @@ export default class ProductDetails {
 
     }
 
-
-    async renderProductDetails() {
-
-
-        document.querySelector('h2').textContent = this.product.Brand.Name;
-        document.querySelector('h3').textContent = this.product.NameWithoutBrand;
-
-        const productImage = document.getElementById('productImage');
-        productImage.src = this.product.Image;
-        productImage.alt = this.product.NameWithoutBrand;
-
-        document.getElementById('productPrice').textContent = this.product.FinalPrice;
-        document.getElementById('productColor').textContent = this.product.Colors[0].ColorName;
-        document.getElementById('productDesc').innerHTML = this.product.DescriptionHtmlSimple;
-
-        document.getElementById('addToCart').dataset.id = this.product.Id;
+    renderProductDetails() {
+        ProductDetailsTemplate(this.product);
     }
-
-
-
 }
+
+function ProductDetailsTemplate(product) {
+    document.querySelector('h2').textContent = product.Category.chartArt(0).toUpperCase() + product.Category.slice(1);
+    document.querySelector('#p-brand').textContent = product.Brand.Name;
+    document.querySelector('#p-name').textContent = product.NameWithoutBrand;
+
+    const productImage = document.getElementById('productImage');
+    productImage.src = product.Image.PrimaryExtraLarge;
+    productImage.alt = product.NameWithoutBrand;
+    const euroPrice = new Intl.NumberFormat('de-DE',
+        {
+            style: 'currency', currency: 'EUR',
+        }).format(Number(product.FinalPrice) * 0.85);
+
+    document.getElementById('productPrice').textContent = `${euroPrice}`;
+    document.getElementById('productColor').textContent = product.Colors[0].ColorName;
+    document.getElementById('productDesc').innerHTML = product.DescriptionHtmlSimple;
+
+    document.getElementById('addToCart').dataset.id = product.Id;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,3 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
-const dataSource = new ProductData("tents");
-const listElement = document.getElementById("listElement");
-const productList = new ProductList("tents", dataSource, listElement);
-
 loadHeaderFooter();
-
-productList.init();
